71f20d551c2396c5568d7aef8f4bbe5c26fc87d7
filoghira <filippo.ghirardini.ra@gmail.com>
2022-03-10 20:33:14 +0100

    First commit

---
+++ Acquario.php
@@ -0,0 +1,123 @@
+<?php
+session_start();
+
+// Ghirardini Filippo 5AIN 10/03/2022
+
+function init($height, $width): array
+{
+
+    $_SESSION['counter'] = 0;
+    $_SESSION['h']=$height;
+    $_SESSION['w']=$width;
+
+    $nFish_1 = 5;
+    $nFih_2 = 5;
+
+    $matrix = array_fill(0, $height, array_fill(0, $width, 0));
+
+    for ($i=0; $i <= $nFish_1; $i++)
+        $matrix[rand(0,$height)][rand(0,$width)] = 1;
+
+    for ($i=0; $i <= $nFih_2; $i++)
+        $matrix[rand(0,$height)][rand(0,$width)] = 2;
+
+    return $matrix;
+}
+
+function move_fishes(){
+    $matrix = $_SESSION['acquario'];
+    $h = $_SESSION['h'];
+    $w = $_SESSION['w'];
+
+    for ($i=0; $i < $h; $i++)
+        for ($j=0; $j < $w; $j++){
+            // Move fishes to a random direction
+            $dir = rand(0, 3);
+            if(valid_direction($i, $j, $dir, $matrix)) {
+
+                switch ($dir) {
+                    // Up
+                    case 0:
+                        $temp = $matrix[$i][$j];
+                        $matrix[$i - 1][$j] = $temp;
+                        break;
+                    // Right
+                    case 1:
+                        $temp = $matrix[$i][$j];
+                        $matrix[$i][$j + 1] = $temp;
+                        break;
+                    // Down
+                    case 2:
+                        $temp = $matrix[$i][$j];
+                        $matrix[$i + 1][$j] = $temp;
+                        break;
+                    // Left
+                    case 3:
+                        $temp = $matrix[$i][$j];
+                        $matrix[$i][$j - 1] = $temp;
+                        break;
+                }
+                $matrix[$i][$j] = 0;
+            }
+
+        }
+
+    $_SESSION['acquario'] = $matrix;
+}
+
+function valid_direction($i, $j, $dir, $matrix): bool
+{
+    if ($matrix[$i][$j] == 0)
+        return false;
+    $h = $_SESSION['h'];
+    $w = $_SESSION['w'];
+
+    return match ($dir) {
+        // Up
+        0 => ($i > 0 && $matrix[$i - 1][$j] == 0),
+        // Right
+        1 => ($j < ($w - 1) && $matrix[$i][$j + 1] == 0),
+        // Down
+        2 => ($i < ($h - 1)) && $matrix[$i + 1][$j] == 0,
+        // Left
+        3 => ($j > 0 && $matrix[$i][$j - 1] == 0),
+        default => false,
+    };
+}
+
+function print_matrix($matrix){
+    echo "<table>";
+    for ($i=0; $i < $_SESSION['h']; $i++) {
+        echo "<tr>";
+        for ($j=0; $j < $_SESSION['w']; $j++)
+            echo "<td><img src='images/".$matrix[$i][$j].".png' height='30' width='30' alt='lol'></td>";
+        echo "</tr>";
+    }
+    echo "</table>";
+}
+
+header("Refresh: 1; URL=".$_SERVER['REQUEST_URI']);
+
+if (!isset($_SESSION['counter']))
+    $_SESSION['acquario'] = init(10,10);
+else {
+    move_fishes();
+    $_SESSION['counter']++;
+}
+?>
+
+<!doctype html>
+<html lang="it">
+<head>
+    <link rel='stylesheet' href='style/style.css'>
+    <title>Acquario</title>
+</head>
+<body>
+<h1>Acquario</h1>
+
+<?php
+print_matrix( $_SESSION['acquario']);
+?>
+
+</body>
+</html>
